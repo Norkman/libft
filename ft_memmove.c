90303dfc93a581b4c52e650ab1c3f3c40720316a
Norkman <norman.nlb@gmail.com>
2021-12-03 17:45:00 +0100

    prout

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,18 +6,17 @@
 /*   By: nle-bret <nle-bret@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 08:01:21 by nle-bret          #+#    #+#             */
-/*   Updated: 2021/12/03 17:01:12 by nle-bret         ###   ########.fr       */
+/*   Updated: 2021/12/03 17:44:27 by nle-bret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "stdio.h"
 
 static void	is_upper(unsigned char *dst2, unsigned char *src2, size_t len)
 {
-	int	i;
+	long int	i;
 
-	i = len - 1;
+	i = (long int)len - 1;
 	while (i >= 0)
 	{		
 		dst2[i] = src2[i];
@@ -27,10 +26,10 @@ static void	is_upper(unsigned char *dst2, unsigned char *src2, size_t len)
 
 static void	is_under_egal(unsigned char *dst2, unsigned char *src2, size_t len)
 {
-	int	i;
+	long int	i;
 
 	i = 0;
-	while ((size_t)i < len)
+	while (i < (unsigned int)len)
 	{
 		dst2[i] = src2[i];
 		i++;
@@ -42,6 +41,8 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	unsigned char	*dst2;
 	unsigned char	*src2;
 
+	if (dst == 0 && src == 0)
+		return (NULL);
 	dst2 = (unsigned char *)dst;
 	src2 = (unsigned char *)src;
 	if (dst2 > src2)
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: nle-bret <nle-bret@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 10:46:28 by nle-bret          #+#    #+#             */
-/*   Updated: 2021/12/03 17:09:10 by nle-bret         ###   ########.fr       */
+/*   Updated: 2021/12/03 17:40:02 by nle-bret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
 	size_t	k;
-	size_t	len_of_little;
+	int		len_of_little;
 	char	*s;
 
-	s = (char *)big;
 	len_of_little = ft_strlen(little);
+	s = (char *)big;
 	i = 0;
-	if (!little)
+	if (little[0] == 0)
 		return (s);
-	while (s[i] && i + len_of_little <= len)
+	while (s[i] && (i + (size_t)len_of_little) <= len)
 	{
 		k = 0;
 		while (s[i + k] == little[k])
